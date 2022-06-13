f7c9a03020c5a0f59a5e0ee97ce10d2e9f423667
Michael Mooney <jmmooney@outlook.com>
2022-06-13 15:55:49 +0100

    [DALA-991] added in initial sandbox containing happy paths

--- sandbox/handlers.js
+++ sandbox/handlers.js
@@ -1,68 +1,94 @@
 "use strict";
 
 const log = require("loglevel");
-
+const organisationTypesResponse = require("./responses/organisation-types_v1.json");
+const searchPostcodeOrPlaceResponse = require("./responses/search-postcode_v2.json");
+const organisationsResponse = require("./responses/organisation_v2.json");
 
 const write_log = (res, log_level, options = {}) => {
-    if (log.getLevel()>log.levels[log_level.toUpperCase()]) {
-        return
-    }
-    if (typeof options === "function") {
-        options = options()
-    }
-    let log_line = {
-        timestamp: Date.now(),
-        level: log_level,
-        correlation_id: res.locals.correlation_id
-    }
-    if (typeof options === 'object') {
-        options = Object.keys(options).reduce(function(obj, x) {
-            let val = options[x]
-            if (typeof val === "function") {
-                val = val()
-            }
-            obj[x] = val;
-            return obj;
-        }, {});
-        log_line = Object.assign(log_line, options)
-    }
-    if (Array.isArray(options)) {
-        log_line["log"] = {log: options.map(x=> {return typeof x === "function"? x() : x })}
-    }
+  if (log.getLevel() > log.levels[log_level.toUpperCase()]) {
+    return;
+  }
+  if (typeof options === "function") {
+    options = options();
+  }
+  let log_line = {
+    timestamp: Date.now(),
+    level: log_level,
+    correlation_id: res.locals.correlation_id,
+  };
+  if (typeof options === "object") {
+    options = Object.keys(options).reduce(function (obj, x) {
+      let val = options[x];
+      if (typeof val === "function") {
+        val = val();
+      }
+      obj[x] = val;
+      return obj;
+    }, {});
+    log_line = Object.assign(log_line, options);
+  }
+  if (Array.isArray(options)) {
+    log_line["log"] = {
+      log: options.map((x) => {
+        return typeof x === "function" ? x() : x;
+      }),
+    };
+  }
 
-    log[log_level](JSON.stringify(log_line))
+  log[log_level](JSON.stringify(log_line));
 };
 
-
 async function status(req, res, next) {
-    res.json({
-        status: "pass",
-        ping: "pong",
-        service: req.app.locals.app_name,
-        version: req.app.locals.version_info
-    });
-    res.end();
-    next();
+  res.json({
+    status: "pass",
+    ping: "pong",
+    service: req.app.locals.app_name,
+    version: req.app.locals.version_info,
+  });
+  res.end();
+  next();
 }
 
-async function hello(req, res, next) {
+async function organisationTypes(req, res, next) {
+  if (req?.query?.["api-version"] !== "1") {
+    res.status(404).json({ statusCode: 404, message: "Resource not found" });
+  } else {
+    res.json(organisationTypesResponse);
+  }
 
-    write_log(res, "warn", {
-        message: "hello world",
-        req: {
-            path: req.path,
-            query: req.query,
-            headers: req.rawHeaders
-        }
-    });
+  res.end();
+  next();
+}
 
+async function searchPostcodeOrPlace(req, res, next) {
+  if (req?.query?.["api-version"] !== "2") {
+    res.status(404).json({ statusCode: 404, message: "Resource not found" });
+  } else if (!req?.query?.["search"]) {
+    // No search parameter is a 404 without a body (this is returned from the backend service)
+    res.status(404);
+  } else {
+    res.status(200).json(searchPostcodeOrPlaceResponse);
+  }
 
-    res.json({message: "hello world"});
-    res.end();
-    next();
+  res.end();
+  next();
+}
+
+async function organisations(req, res, next) {
+  if (req?.query?.["api-version"] !== "2") {
+    res.status(404).json({ statusCode: 404, message: "Resource not found" });
+  } else {
+    res.status(200).json(organisationsResponse);
+  }
+
+  res.end();
+  next();
 }
 
 module.exports = {
-    status: status,
-    hello: hello
+  status: status,
+  organisationTypes,
+  searchPostcodeOrPlace,
+  organisations,
 };
