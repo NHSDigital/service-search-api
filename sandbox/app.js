c04800d35a8595351f7e487c8366c8355f52eeaf
Michael Mooney <jmmooney@outlook.com>
2022-07-09 22:51:06 +0100

    [DALA-1046] Fix cors issues with sandbox

--- sandbox/app.js
+++ sandbox/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const log = require("loglevel");
 const uuid = require("uuid");
 const path = require("path");
@@ -128,6 +129,22 @@ function on_error(err, req, res, next) {
 }
 
 const handlers = require("./handlers");
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: [
+      "origin",
+      "x-requested-with",
+      "accept",
+      "content-type",
+      "nhsd-session-urid",
+      "nhsd-user-identity",
+      "x-correlation-id",
+    ],
+    maxAge: 3628800,
+    methods: ["GET", "PUT", "POST", "DELETE"],
+  })
+);
 app.use(before_request);
 app.get("/_ping", handlers.status);
 app.get("/_status", handlers.status);
