36a00f3fda7d2dfc5724c74735392ab68e824237
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-01-23 14:04:21 +0000

    AMB-1381: Fixed re-written hostname prefix

--- proxies/live/apiproxy/resources/jsc/RewriteHostName.js
+++ proxies/live/apiproxy/resources/jsc/RewriteHostName.js
@@ -8,7 +8,7 @@ if (json.place) {
     // Annoyingly, Apigee's version of JS doesn't support regex look-behinds,
     // so we grab the env string including https:// prefix
     var hostname_regex = /(^https:\/\/[a-zA-Z\-]+)(?=(\.api\.service\.nhs\.uk))/gm;
-    var rewritten_url = value.url.replace(hostname_regex, request_hostname);
+    var rewritten_url = value.url.replace(hostname_regex, "https://" + request_hostname);
     value.url = rewritten_url;
     value.url = value.url.replace("api-version=1", "api-version=2");
     value.url = value.url.replace("service-search", "service-search-api");
