71fd9373f50a85f08083e578945fdf4d867705c1
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-01-23 12:37:13 +0000

    AMB-1381: Added regex to more accurately extract and replace hostname env

--- proxies/live/apiproxy/resources/jsc/CacheRequestHost.js
+++ proxies/live/apiproxy/resources/jsc/CacheRequestHost.js
@@ -1,1 +1,3 @@
-context.setVariable("request_hostname", request.headers.host);
+const env_regex = /^([a-zA-Z]+)(?=(\.api\.service\.nhs\.uk))/gm
+var hostname_env = str.replace(env_regex, request.headers.host)
+context.setVariable("request_hostname_env", hostname_env);
