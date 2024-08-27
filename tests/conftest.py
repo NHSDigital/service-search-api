dd4410b21669f31d70102b682ec151c50ca64927
rami yasir <rami.yasir1@nhs.net>
2024-08-27 16:22:44 +0100

    move tests/conftest.py from apim-test-utils to pytest-nhsd-apim

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,30 +3,43 @@ import asyncio
 import uuid
 
 import pytest
-# from api_test_utils.api_test_session_config import APITestSessionConfig
-# from api_test_utils.apigee_api_apps import ApigeeApiDeveloperApps
-# from api_test_utils.api_session_client import APISessionClient
-
+from pytest_nhsd_apim.apigee_apis import ApigeeNonProdCredentials, ApigeeClient, DeveloperAppsAPI
 from tests.configuration.config import ENVIRONMENT
 
 
-@pytest.fixture(scope='session')
-def api_test_config() -> APITestSessionConfig:
-    """
-        this imports a 'standard' test session config,
-        which builds the proxy uri
-
-    """
-    return APITestSessionConfig()
+@pytest.fixture(scope="session")
+def client():
+    config = ApigeeNonProdCredentials() # should use exported token
+    client = ApigeeClient(config=config)
+    return client
 
 
-@pytest.fixture(scope='function')
-async def api_client(api_test_config: APITestSessionConfig):
-    session_client = APISessionClient(api_test_config.base_uri)
+@pytest.fixture(scope="session")
+def default_apigee_app_reponse(client):
+    apigee_app = DeveloperAppsAPI(client=client)
+    body = {
+        "apiProducts": ["service-search-api-internal-dev"],
+        "attributes": [],
+        "callbackUrl": "example.com",
+        "name": "myapp_test",
+        "scopes": [],
+        "status": "approved"
+    }
 
-    yield session_client
+    response = apigee_app.create_app(email="andrew.littlewood1@nhs.net", body=body)
+    yield response["credentials"][0]
 
-    await session_client.close()
+    print("\nDestroying Default app")
+    apigee_app.delete_app_by_name(email="andrew.littlewood1@nhs.net", app_name="myapp_test")
+
+
+@pytest.fixture(scope="session")
+async def get_api_key(default_apigee_app_reponse):
+    if "sandbox" in ENVIRONMENT:
+        # Sandbox environments don't need authentication. Return fake one
+        return {"apikey": "not_needed"}
+
+    return {"apikey": default_apigee_app_reponse["consumerKey"]}
 
 
 @pytest.fixture(scope="session")
@@ -34,27 +47,6 @@ def event_loop(request):
     loop = asyncio.new_event_loop()
     yield loop
     loop.close()
-
-
-@pytest.fixture(scope="session")
-async def default_apigee_app():
-    apigee_app = ApigeeApiDeveloperApps()
-    await apigee_app.create_new_app()
-
-    yield apigee_app
-
-    # Teardown
-    print("\nDestroying Default App..")
-    await apigee_app.destroy_app()
-
-
-@pytest.fixture()
-async def get_api_key(default_apigee_app):
-    if "sandbox" in ENVIRONMENT:
-        # Sandbox environments don't need authentication. Return fake one
-        return {"apikey": "not_needed"}
-
-    return {"apikey": default_apigee_app.client_id}
 
 
 def make_headers(api_key):
