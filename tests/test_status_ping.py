ce9834b900ade66251486d477f7e9e0208c27608
rami yasir <rami.yasir1@nhs.net>
2024-09-06 14:13:33 +0100

    change status_is_secured test to ensure a 401 response

--- tests/test_status_ping.py
+++ tests/test_status_ping.py
@@ -1,10 +1,6 @@
 import pytest
 import requests
 from .configuration import config
-from .conftest import make_headers
-
-
-API_KEY = "3fed41a7-1de3-4e03-980b-5945a4c06c86"
 
 
 @pytest.mark.e2e
@@ -18,15 +14,10 @@ def test_ping():
     assert "version" in ping_data
 
 
-# THIS DOES NOT WORK AS INTENDED.
-# For some reason, only the special API_KEY constant above works with this proxy endpoint. It looks like
-# an app ID to me, but what do I know? It seems to work, insofar as it can reach the endpoint, but then the
-# status is a fail because we don't have a valid key for an active subscription (whatever that means)
+@pytest.mark.e2e
+@pytest.mark.smoketest
 def test_status_is_secured():
     resp = requests.get(
         url=f"{config.BASE_URL}/{config.BASE_PATH}/_status",
-        headers=make_headers(API_KEY),
     )
-    assert resp.status_code == 200
-    # status_data = resp.json()
-    # assert status_data["status"] == "healthy"
+    assert resp.status_code == 401
