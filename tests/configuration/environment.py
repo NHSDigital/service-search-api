2c2ff5b6ea2a9effcd6ebbe0fbe121c5bdbdde37
rami yasir <rami.yasir1@nhs.net>
2024-09-17 12:00:14 +0100

    more experimentation with environment variables

--- tests/configuration/environment.py
+++ tests/configuration/environment.py
@@ -19,5 +19,8 @@ ENV = {
     # Apigee
     "environment": get_env("APIGEE_ENVIRONMENT"),
     "base_path": get_env("SERVICE_BASE_PATH"),
-    "status_api_key": get_env("STATUS_ENDPOINT_API_KEY")
+    "status_api_key": get_env("STATUS_ENDPOINT_API_KEY"),
+    "apigee_username": get_env("APIGEE_USERNAME"),
+    "apigee_password": get_env("APIGEE_PASSWORD"),
+    "apigee_otp_key": get_env("APIGEE_OTP_KEY")
 }
