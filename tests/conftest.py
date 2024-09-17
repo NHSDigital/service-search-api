d068733100579e6fa2f4671dde5d2bfb25f42d98
rami yasir <rami.yasir1@nhs.net>
2024-09-17 11:03:04 +0100

    see if adding env variables from integration into python tests works

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 import asyncio
+import os
 import uuid
 import pytest
 from pytest_nhsd_apim.apigee_apis import ApigeeNonProdCredentials, ApigeeClient, DeveloperAppsAPI
@@ -8,7 +9,13 @@ from tests.configuration.config import ENVIRONMENT
 
 @pytest.fixture(scope="session")
 def client():
-    config = ApigeeNonProdCredentials() # should use exported token
+    # Can I check environment and use particular env variables depending on that?
+    # How do I check the environment?
+    config = ApigeeNonProdCredentials(
+        apigee_nhsd_nonprod_password=os.environ["APIGEE_PASSWORD"],
+        apigee_nhsd_nonprod_username=os.environ["APIGEE_USERNAME"],
+        apigee_nhsd_nonprod_otp_key=os.environ["APIGEE_OTP_KEY"]
+        ) # should use exported token
     client = ApigeeClient(config=config)
     return client
 
