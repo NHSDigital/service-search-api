d70bfbd8dcad2d3432b867288d53b81e04a42fd2
rami yasir <rami.yasir1@nhs.net>
2024-09-27 12:37:11 +0100

    rewrite ping tests and alter manifest_template rate limit

--- tests/test_status_ping.py
+++ tests/test_status_ping.py
@@ -1,33 +1,39 @@
-# import pytest
-# import requests
-# from assertpy import assert_that
-# from .configuration import config
-# from .conftest import make_headers
+import pytest
+import requests
+from .configuration import config
 
 
-# @pytest.mark.e2e
-# @pytest.mark.smoketest
-# def test_ping():
-#     resp = requests.get(
-#         url=f"{config.BASE_URL}/{config.BASE_PATH}/_ping",
-#     )
-#     assert resp.status_code == 200
-#     ping_data = resp.json()
-#     assert "version" in ping_data
+@pytest.mark.integration
+@pytest.mark.e2e
+@pytest.mark.smoketest
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
+def test_ping():
+    resp = requests.get(
+        url=f"{config.BASE_URL}/{config.BASE_PATH}/_ping",
+    )
+    assert resp.status_code == 200
+    ping_data = resp.json()
+    assert "version" in ping_data
 
 
-# @pytest.mark.e2e
-# @pytest.mark.smoketest
-# def test_status_is_secured():
-#     resp = requests.get(
-#         url=f"{config.BASE_URL}/{config.BASE_PATH}/_status",
-#     )
-#     assert resp.status_code == 401
+@pytest.mark.integration
+@pytest.mark.e2e
+@pytest.mark.smoketest
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
+def test_status_is_secured():
+    resp = requests.get(
+        url=f"{config.BASE_URL}/{config.BASE_PATH}/_status",
+    )
+    assert resp.status_code == 401
 
 
-# def test_for_status():
-#     resp = requests.get(
-#         url=f"{config.BASE_URL}/{config.BASE_PATH}/_status",
-#         headers=make_headers(config.STATUS_ENDPOINT_API_KEY)
-#     )
-#     assert resp.status_code == 200
+@pytest.mark.integration
+@pytest.mark.e2e
+@pytest.mark.smoketest
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
+def test_for_status(status_endpoint_auth_headers):
+    resp = requests.get(
+        url=f"{config.BASE_URL}/{config.BASE_PATH}/_status",
+        headers=status_endpoint_auth_headers
+    )
+    assert resp.status_code == 200
