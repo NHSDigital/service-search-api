77f02560c82ea88dce4359b53178562fb972cbfd
rami yasir <rami.yasir@nhschoices.net>
2025-02-20 16:48:30 +0000

    alter status test to access apim response code as well as apigee status code

--- tests/test_status_ping.py
+++ tests/test_status_ping.py
@@ -6,7 +6,6 @@ from .configuration import config
 @pytest.mark.integration
 @pytest.mark.e2e
 @pytest.mark.smoketest
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
 def test_ping():
     resp = requests.get(
         url=f"{config.BASE_URL}/{config.BASE_PATH}/_ping",
@@ -24,6 +23,7 @@ def test_status_is_secured():
     resp = requests.get(
         url=f"{config.BASE_URL}/{config.BASE_PATH}/_status",
     )
+    print(resp.status_code)
     assert resp.status_code == 401
 
 
@@ -36,4 +36,10 @@ def test_for_status(status_endpoint_auth_headers):
         url=f"{config.BASE_URL}/{config.BASE_PATH}/_status",
         headers=status_endpoint_auth_headers
     )
-    assert resp.status_code == 200
+
+    status_data = resp.json()
+    apim_responde_code = status_data["checks"]["healthcheck"]["responseCode"]
+    apigee_status_code = resp.status_code
+    
+    assert apigee_status_code == 200
+    assert apim_responde_code == 200
